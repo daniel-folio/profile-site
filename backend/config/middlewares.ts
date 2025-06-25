072aea8cc7f9a9e56acf02bcb646a645276fd967
daniel-folio <daniel.devfolio@gmail.com>
2025-06-25 17:46:36 +0900

    Fix: Update middleware for Cloudinary preview

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,9 +1,21 @@
 export default [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'] ,
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'], // 여기에 Cloudinary 주소 추가
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'], // 여기에 Cloudinary 주소 추가
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
