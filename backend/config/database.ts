7634332e9c19e6af529b4793cf5a1015e207f417
daniel-folio <daniel.devfolio@gmail.com>
2025-07-23 20:04:13 +0900

    로컬에서도 Neon 연결할 수 있도록 설정 변경

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,25 +1,40 @@
 import path from 'path';
+const parse = require('pg-connection-string').parse;
 
 export default ({ env }) => {
-  // 프로덕션 환경(Render)에서는 PostgreSQL(Neon)을 사용합니다.
-  if (env('NODE_ENV') === 'production') {
+   // DATABASE_URL 환경 변수가 존재할 경우 (배포 서버 실행 시)
+  if (env('DATABASE_URL')) {
+    const config = parse(env('DATABASE_URL'));
     return {
       connection: {
         client: 'postgres',
         connection: {
-          connectionString: env('DATABASE_URL'),
-          ssl: { rejectUnauthorized: false },
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          user: config.user,
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: false,
+          },
         },
         debug: false,
       },
     };
   }
-  // 그 외, 로컬 개발 환경에서는 SQLite를 사용합니다.
+
+  // DATABASE_URL 환경 변수가 없을 경우 (로컬 또는 빌드 시)
+  // 로컬에서는 .env 파일의 SQLite 설정을, 빌드 시에는 가짜 설정으로 사용
   return {
     connection: {
       client: 'sqlite',
       connection: {
-        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        filename: path.join(
+          __dirname,
+          '..',
+          '..',
+          env('DATABASE_FILENAME', '.tmp/data.db')
+        ),
       },
       useNullAsDefault: true,
     },
